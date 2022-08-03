fe09ac294a7d48a596084e17b51a06ebcdaae137
Danilo Barreto <dgbarreto@gmail.com>
2022-08-03 09:11:54 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Teams"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () => _launchUrl(),
+            child: const Text("Open Teams"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _launchUrl() async {
+    Uri url = Uri(scheme: "msteams");
+    await launchUrl(url, mode: LaunchMode.externalNonBrowserApplication);
+  }
+}
